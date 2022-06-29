68127ff37ef5653c801cfc9c6e9b79db27b0b644
Bachir <bachirmdicko@gmail.com>
2022-06-29 10:12:04 -0400

    Completed

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(location, left, bottom){
+    let thing = document.createElement('img')
+    thing.src = location
+    thing.style.position = 'fixed'
+    thing.style.left = left + 'px'
+    thing.style.bottom = bottom + 'px'
+    document.body.append(thing)
+    return thing
+}
+
+function newItem(location, left, bottom){
+    let thing = newImage(location, left, bottom);
+
+    thing.addEventListener('dblclick', function () {
+        thing.remove()
+    })
+}
+
+
+
+newImage("assets/green-character.gif", 100, 100);
+newImage("assets/pine-tree.png", 450, 200);
+newImage("assets/tree.png", 200, 300);
+newImage("assets/pillar.png", 350, 100);
+newImage("assets/crate.png", 150, 200);
+newImage("assets/well.png", 500, 425);
+
+newItem("assets/sword.png", 500, 405);
+newItem("assets/sheild.png", 165, 185);
+newItem("assets/staff.png", 600, 100);
+
+
